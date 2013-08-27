ca8e8e7cb099aae328162fad2bb40e79d03e2ad1
Gabe Gorelick <gabriel.gorelick@jhuapl.edu>
2013-08-27 12:25:52 -0400

    Clean up setup SQL script
    * Prefix objects with `oe` schema
    * Fix topology schema issue
    * Add salt and algorithm columns to user
    * Change date functions to use timestamptz
    * Mark date functions as stable

--- core/db/V1__setup.sql
+++ core/db/V1__setup.sql
@@ -1,6 +1,14 @@
 /**
  * Set up a (PostgreSQL) database for OpenEssence.
  *
+ * Prerequisites:
+ * 1. Create an oe user (CREATE USER oe WITH PASSWORD 'PASSWORD')
+ * 2. Create database (CREATE DATABASE openessence)
+ * 3. Grant privileges to oe (GRANT ALL PRIVILEGES ON DATABASE openessence TO oe). Feel free to grant finer grained
+ *     permissions (e.g. readonly on public) if you want more security.
+ * 4. Create oe schema (CREATE SCHEMA oe AUTHORIZATION oe)
+ * 5. Run this script
+ *
  * Note that there is some flexibility in terms of what objects OE expects a
  * DB to have. For example, you can call your user table whatever you want, as
  * long as it's mapped correctly in the ORM layer. However, try to stick to the
@@ -9,20 +17,25 @@
 BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS postgis SCHEMA public;
-CREATE EXTENSION IF NOT EXISTS postgis_topology SCHEMA topology;
 
---if not exists for schemas is only in Postgres 9.3 devel
---create schema if not exists oe;
+-- postgis_topology creates the topology schema
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
 
-ALTER ROLE postgres SET search_path = oe, public;
+-- if not exists for schemas is only in Postgres 9.3 devel
+-- create schema if not exists oe;
+
+-- if you're lazy and didn't make a dedicated oe user, run this to add the oe schema to the search path
+-- ALTER ROLE postgres SET search_path = oe, public;
 
 /*****************************************************************************
  * Objects required by OpenESSENCE
  *****************************************************************************/
-CREATE TABLE "user" (
+CREATE TABLE oe."user" (
   id                      SERIAL PRIMARY KEY,
   name                    TEXT    NOT NULL UNIQUE,
   password                TEXT    NOT NULL,
+  salt                    TEXT    NOT NULL,
+  algorithm               TEXT    NOT NULL,
   enabled                 BOOLEAN NOT NULL DEFAULT FALSE,
   non_expired             BOOLEAN NOT NULL DEFAULT TRUE,
   credentials_non_expired BOOLEAN NOT NULL DEFAULT TRUE,
@@ -30,25 +43,25 @@ CREATE TABLE "user" (
 );
 
 -- add admin/admin login
-INSERT INTO "user" (name, password, enabled, non_expired, credentials_non_expired, account_non_locked)
-  VALUES ('admin', '805c31ff4a51bb8641c1fd00ce3eccb680e49be3', TRUE, TRUE, TRUE, TRUE);
+INSERT INTO oe."user" (name, password, salt, algorithm, enabled, non_expired, credentials_non_expired, account_non_locked)
+  VALUES ('admin', '805c31ff4a51bb8641c1fd00ce3eccb680e49be3', 'bACzDYExfadmin203948576', 'SHA-1', TRUE, TRUE, TRUE, TRUE);
 
-CREATE TABLE role (
+CREATE TABLE oe.role (
   id          TEXT NOT NULL PRIMARY KEY,
   description TEXT
 );
-INSERT INTO role VALUES ('ROLE_ADMIN', 'Access to applications management features');
-INSERT INTO role VALUES ('ROLE_USER', 'Access to data entry and views');
+INSERT INTO oe.role VALUES ('ROLE_ADMIN', 'Access to applications management features');
+INSERT INTO oe.role VALUES ('ROLE_USER', 'Access to data entry and views');
 
-CREATE TABLE user_role (
+CREATE TABLE oe.user_role (
   user_id INTEGER NOT NULL REFERENCES "user" (id),
   role_id TEXT    NOT NULL REFERENCES role (id),
   CONSTRAINT user_role_pkey PRIMARY KEY (user_id, role_id)
 );
-INSERT INTO user_role VALUES (1, 'ROLE_USER');
-INSERT INTO user_role VALUES (1, 'ROLE_ADMIN');
+INSERT INTO oe.user_role VALUES (1, 'ROLE_USER');
+INSERT INTO oe.user_role VALUES (1, 'ROLE_ADMIN');
 
-CREATE TABLE saved_query (
+CREATE TABLE oe.saved_query (
   query_id    SERIAL  NOT NULL PRIMARY KEY,
   query_name  TEXT    NOT NULL,
   user_id     INTEGER NOT NULL REFERENCES "user" (id),
@@ -58,7 +71,7 @@ CREATE TABLE saved_query (
   CONSTRAINT saved_query_user_id_query_name_key UNIQUE (user_id, query_name)
 );
 
-CREATE OR REPLACE FUNCTION getweekofyear(TIMESTAMP WITHOUT TIME ZONE, INTEGER DEFAULT 1)
+CREATE OR REPLACE FUNCTION oe.getweekofyear(TIMESTAMPTZ, INTEGER DEFAULT 1)
   RETURNS DOUBLE PRECISION AS
   $BODY$
   DECLARE
@@ -67,7 +80,7 @@ CREATE OR REPLACE FUNCTION getweekofyear(TIMESTAMP WITHOUT TIME ZONE, INTEGER DE
     ms1d    INTEGER;
     ms7d    INTEGER;
     _offset INTEGER;
-    D3      TIMESTAMP WITHOUT TIME ZONE;
+    D3      TIMESTAMPTZ;
     DC3     REAL;
     AWN     REAL;
     Wyr     INTEGER;
@@ -78,28 +91,28 @@ CREATE OR REPLACE FUNCTION getweekofyear(TIMESTAMP WITHOUT TIME ZONE, INTEGER DE
 -- milliseconds in a day
     ms7d := 7 * ms1d;
 -- milliseconds in a week
-               -- Used to compute offset based on epi week start day (0 sunday -> 6 saturday), defaults to 1
+ -- Used to compute offset based on epi week start day (0 sunday -> 6 saturday), defaults to 1
     _offset := (((abs(oset - 6)) + 5) % 7);
 -- epi week start day offset
 
     D3 := (dt :: DATE) + _offset;
     DC3 := (EXTRACT(EPOCH FROM D3) * 1000) / ms1d;
-
     AWN := floor(DC3 / 7);
 -- an Absolute Week Number
-    Wyr := EXTRACT(YEAR FROM (TIMESTAMP WITHOUT TIME ZONE 'epoch' + (AWN * ms7d / 1000) * INTERVAL '1 second'));
+
+    Wyr := EXTRACT(YEAR FROM (TIMESTAMPTZ 'epoch' + (AWN * ms7d / 1000) * INTERVAL '1 second'));
 
 -- first day of this year as milliseconds
     ms1dy := EXTRACT(EPOCH FROM to_date(((Wyr :: VARCHAR) || '-01-07'), 'YYYY-MM-DD') :: TIMESTAMPTZ) * 1000;
 
     result := (AWN - floor(ms1dy / ms7d) + 1);
     RETURN result;
-
   END;
   $BODY$
-LANGUAGE plpgsql VOLATILE;
+-- STABLE because time zones can change
+LANGUAGE 'plpgsql' STABLE;
 
-CREATE OR REPLACE FUNCTION getyear(TIMESTAMP WITHOUT TIME ZONE, INTEGER DEFAULT 1)
+CREATE OR REPLACE FUNCTION oe.getyear(TIMESTAMPTZ, INTEGER DEFAULT 1)
   RETURNS DOUBLE PRECISION AS
   $BODY$
   DECLARE
@@ -108,7 +121,7 @@ CREATE OR REPLACE FUNCTION getyear(TIMESTAMP WITHOUT TIME ZONE, INTEGER DEFAULT 
     ms1d    INTEGER;
     ms7d    INTEGER;
     _offset INTEGER;
-    D3      TIMESTAMP WITHOUT TIME ZONE;
+    D3      TIMESTAMPTZ;
     DC3     REAL;
     AWN     REAL;
     Wyr     DOUBLE PRECISION;
@@ -117,31 +130,18 @@ CREATE OR REPLACE FUNCTION getyear(TIMESTAMP WITHOUT TIME ZONE, INTEGER DEFAULT 
 -- milliseconds in a day
     ms7d := 7 * ms1d;
 -- milliseconds in a week
-       -- Used to compute offset based on epi week start day (0 sunday -> 6 saturday), defaults to 1
+      -- Used to compute offset based on epi week start day (0 sunday -> 6 saturday), defaults to 1
     _offset := (((abs(oset - 6)) + 5) % 7);
 -- epi week start day offset
     D3 := (dt :: DATE) + _offset;
     DC3 := (EXTRACT(EPOCH FROM D3) * 1000) / ms1d;
     AWN := floor(DC3 / 7);
 -- an Absolute Week Number
-    Wyr := EXTRACT(YEAR FROM (TIMESTAMP WITHOUT TIME ZONE 'epoch' + (AWN * ms7d / 1000) * INTERVAL '1 second'));
+    Wyr := EXTRACT(YEAR FROM (TIMESTAMPtz 'epoch' + (AWN * ms7d / 1000) * INTERVAL '1 second'));
     RETURN Wyr;
   END;
   $BODY$
-LANGUAGE 'plpgsql' VOLATILE;
-
-CREATE OR REPLACE FUNCTION getstartdateofweek(TIMESTAMP WITHOUT TIME ZONE, INTEGER DEFAULT 1)
-  RETURNS TIMESTAMP WITHOUT TIME ZONE AS
-  $BODY$
-  DECLARE
-    dt ALIAS FOR $1;
-    oset ALIAS FOR $2;
-    t INTEGER;
-  BEGIN
-    t := (abs(Extract(DOW FROM (dt :: DATE - oset)) - 6) - 6) :: INTEGER % 7;
-    RETURN dt :: DATE + t;
-  END;
-  $BODY$
-LANGUAGE 'plpgsql' VOLATILE;
+-- STABLE because time zones can change
+LANGUAGE 'plpgsql' STABLE;
 
 COMMIT;
